de41c8f6dc42bad7c5bcc32a05ed67d91f913006
rohit madhu <rohitmadhu@rohits-MacBook-Pro.local>
2017-09-07 15:15:42 +0530

    first commit

---
+++ process.php
@@ -0,0 +1,42 @@
+
+<html>
+<head>
+	<title>1st page</title>
+</head>
+<body>
+<?php
+echo "hello";
+//get values from html
+$username = $_POST['user'];
+$password = $_POST['pass'];
+
+//to prevent mysql injection
+$username = stripcslashes($username);
+$password =stripcslashes($password);
+$username = mysql_real_escape_string($username);
+$password = mysql_real_escape_string($password);
+
+//connect
+$user = 'root';
+$password = 'root';
+$db = 'login';
+$host = 'localhost';
+$port = 8889;
+$link = mysql_connect("$host:$port",$user,$password);
+$db_selected = mysql_select_db($db,$link);
+//query
+$result= mysql_query("select * from users where username = '$username' and password ='password'");
+            or die("Failed to query database ".mysql_error());
+
+$row = mysql_fetch_array($result);
+if($row['username']) == $username && $row['password'] == $password )
+{
+	echo "Login success!!! Welcome" . $row['username'];
+}
+else{
+	echo "Failed to login!";
+}
+
+?>
+</body>
+</html>
